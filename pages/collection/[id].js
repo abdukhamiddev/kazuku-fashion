4bff59021a0bab78fd8895bffd2ffbef3b3155b0
abdukhamiddev <latipovabdukhamid@gmail.com>
2022-01-03 11:05:15 +0500

    singlecollection

--- pages/collection/[id].js
+++ pages/collection/[id].js
@@ -1,13 +1,115 @@
-import { useRouter } from "next/router";
-import React from "react";
-
-const SingleCollection = () => {
-	const { query } = useRouter();
+/* eslint-disable @next/next/no-img-element */
+import axios from "axios";
+import Link from "next/link";
+import Layout from "../../components/Layout";
+import Meta from "../../components/Meta";
+import SpanText from "../../components/SpanText";
+import { Link as Anchor } from "react-scroll";
+function Collection({ collection }) {
 	return (
-		<div>
-			<h1 className="text-[#cfcfcf]">Hi {query.id}</h1>
-		</div>
+		<>
+			<Meta
+				titleShort={`KAZUKI | ${collection.titleENG}`}
+				titleLong={`KAZUKI | ${collection.titleENG} Collection`}
+				description={`KAZUKI | ${collection.titleENG}. ${collection.about}`}
+				keywords="KAZUKU, Women's Clothing Collections, Best women's clothing 2021-2022, Moda 2021, 2022"
+			/>
+			<Layout>
+				<section className="collection" id="collection">
+					<div className="collection__h">
+						<div className="collection__name">
+							<div className="collection__thumb">
+								<img src={collection.coverImg} alt={collection.titleENG} />
+							</div>
+							<div id="h1" className="collection__header-text">
+								<div className="str1">
+									<h2 className="fk-h1 logo">
+										KA <span className="n">ZU</span>KU/
+									</h2>
+									<h1>{collection.titleENG}</h1>
+								</div>
+								<div className="str2">
+									<h5>{collection.by}</h5>
+								</div>
+								<div className="str3">
+									<p>{collection.about}</p>
+									<p>{collection.about}</p>
+								</div>
+								<div className="time">{collection.release}</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<section id="collection-collage" className="collage">
+					<img src={collection.collage} alt={collection.titleENG} />
+					<div className="lein">
+						<div className="lineimg"></div>
+					</div>
+					<section className="about" id="about">
+						<h6>
+							The New Collection <br /> / S - J 2022
+						</h6>
+						<h2 className="fk-h1">{collection.about}</h2>
+					</section>
+				</section>
+				<div className="anchors">
+					<div>
+						<Link href="/">
+							<a>
+								<SpanText text="Go to Collections" />
+							</a>
+						</Link>
+						<Anchor to="collection" smooth={true} duration={2600}>
+							<span className="two">
+								<SpanText text="Go to top" />
+							</span>
+						</Anchor>
+						<Link href="https://github.com/abdukhamiddev">
+							<a className="two">
+								<SpanText text="GitHub" />
+							</a>
+						</Link>
+					</div>
+					<div className="year">{new Date().getFullYear()}</div>
+				</div>
+			</Layout>
+		</>
 	);
+}
+
+export default Collection;
+
+export const getStaticProps = async (ctx) => {
+	try {
+		const res = await axios.get(
+			`${process.env.API_URL}/api/v.1.0/get-collections/${ctx.params.id}`
+		);
+		const collection = await res.data;
+		return {
+			props: {
+				collection,
+			},
+		};
+	} catch (error) {
+		console.log(error);
+	}
 };
 
-export default SingleCollection;
+export const getStaticPaths = async (ctx) => {
+	try {
+		const res = await axios.get(
+			`${process.env.API_URL}/api/v.1.0/get-collections`
+		);
+
+		const { collections } = await res.data;
+		const paths = await collections.map(({ id }) => {
+			return { params: { id: id } };
+		});
+		return {
+			paths,
+			fallback: false,
+		};
+	} catch (error) {
+		console.log(error);
+	}
+};
